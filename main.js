8c4c6f417a39ae249be4eb2ff3b39640cdf47a74
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-03-08 20:02:15 +0530

    Update Mongo URI

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ io.on('connection', async(socket) => {
 });
 
 try {
-    await mongoose.connect('mongodb://localhost:27017/Company')
+    await mongoose.connect(process.env.MONGO_URI)
         .then(e => { console.log("connected to DB!") })
         .catch(e => { console.log("failed to connect: " + e) })
 } catch (error) {
